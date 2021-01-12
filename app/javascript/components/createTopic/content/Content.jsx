d9a2b5980f779cf2c8f981bc8d2c4170d718e1fd
sarath029 <sarathm010@gmail.com>
2021-01-12 22:04:29 +0530

    Add validation for topic creation

--- app/javascript/components/createTopic/content/Content.jsx
+++ app/javascript/components/createTopic/content/Content.jsx
@@ -2,31 +2,45 @@ import React, { useEffect, useState } from 'react';
 import { Card, Container, Col, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-import { useHistory, useParams, withRouter} from "react-router-dom";
+import { useHistory, useParams, withRouter } from "react-router-dom";
 import { connect } from 'react-redux'
+import { useAlert } from 'react-alert'
 
 const Content = (props) => {
     let { id } = useParams();
-    const [subject, setSubject] = useState();
-    const [content, setContent] = useState();
+    const [subject, setSubject] = useState("");
+    const [content, setContent] = useState("");
 
+    const alert = useAlert();
     const history = useHistory();
-    const addTopic = (event) => {
-        const devices_url = '/api/v1/topics/';
-        const device = {
 
-            "topic": {
-                "creator_id": props.currentUser,
-                "subject": subject,
-                "content": content
+    const performValidation = () => {
+        return content.length > 0 && subject.length > 0;
+    }
+    const addTopic = (event) => {
+
+        if (performValidation()) {
+            const devices_url = '/api/v1/topics/';
+            const device = {
+
+                "topic": {
+                    "creator_id": props.currentUser,
+                    "subject": subject,
+                    "content": content
+                }
+
             }
 
+            axios.post(devices_url, device)
+                .then(comment_response => {
+                    history.push("/topics");
+                });
+        }
+        else{
+
+            alert.show('please enter subject and content')
         }
 
-        axios.post(devices_url, device)
-            .then(comment_response => {
-                history.push("/topics");
-            });
     }
 
     return (
@@ -35,7 +49,7 @@ const Content = (props) => {
                 <Card className="mt-3">
                     <Card.Body className="p-5">
                         <h6>Create Topic</h6>
-                        <input style={{ width: "100%", marginTop: "10px" }} type="text" placeholder="Subject"  onChange={e => setSubject(e.target.value)}></input>
+                        <input style={{ width: "100%", marginTop: "10px" }} type="text" placeholder="Subject" onChange={e => setSubject(e.target.value)}></input>
                         <textarea style={{ width: "100%", height: "200px", marginTop: "10px" }} type="textarea" onChange={e => setContent(e.target.value)}></textarea>
                         <Button variant="outline-primary" onClick={addTopic} style={{ marginLeft: "90%" }}>Submit</Button>
                     </Card.Body>
